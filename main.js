fd36179ee098f6810875101fc8d1666f74bf343a
Fairbuyng <emijere.richard@gmail.com>
2022-02-08 21:09:33 +0100

    First learning

---
+++ main.js
@@ -0,0 +1,67 @@
+// let a = Math.floor(Math.random() * 100)
+// let b = Math.floor(Math.random() * 100)
+
+// if (a > b) {
+//     console.log("a is greater")
+//     console.log(a)
+//     console.log(b)
+// }
+
+// const dayOfWeek = "Monday";
+
+// if (dayOfWeek === "Monday") {
+//   console.log("Fuck Mondays!");
+// } else if (dayOfWeek === "Saturdays") {
+//   console.log("Make We Rest Abeg!");
+// }
+
+// const age = 18;
+
+// if (age < 18) {
+//   console.log("You are not allowed");
+// } else if (age < 18) {
+//   console.log("You are allowed!");
+// } else if(age < 50) {
+//   console.log("You be Agba!");
+// }
+
+// const password = prompt("Enter a Password:");
+
+// if (password.length >= 8) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("Good job, No space!");
+//   } else {
+//     console.log("Password cannot contain space");
+//   }
+// } else {
+//   console.log("Password not up to 8 characters");
+// }
+
+// const userInput = prompt("Enter a number");
+
+// if (userInput) {
+//   console.log("Response taken");
+// } else {
+//   console.log("You cannot enter nothing");
+// }
+
+// const password = prompt("Enter a password");
+
+// if (password.length >= 8 && password.indexOf(" ") === -1) {
+//   console.log("Password accepted");
+// } else {
+//   console.log("Invalid format for a password")
+// }
+
+// const age = prompt("Enter your age");
+
+// if (age < 18) {
+//   console.log("You're not allowed")
+// } else if (age >= 18) {
+//   console.log("You're accepted!")
+// } else if (age >= 65) {
+//   console.log("You're too old for this!'")
+//
+
+// Write - Your - Code
+// let rawPrice = Number(prompt
